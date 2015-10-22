15bdfa5a6ee7b93acaf576abf3dbdd0cb0e8d497
Tamilmathi <devishri75@yahoo.in>
2015-10-22 16:42:26 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,8 +1,8 @@
 <?php
 include('connection.php');
-$result="UPDATE Employee 
+//$result="UPDATE Employee 
         SET (fname='$fname',lname='$lname',mail='$mail',cmail='cmail',gender='$gender',age='$age',address='$address',states='$states',number='$number' 
          WHERE id='$id') ";
-mysqli_query($connection,$result);
-header("Location:edit.php");
+//mysqli_query($connection,$result);
+//header("Location:edit.php");
 ?>
